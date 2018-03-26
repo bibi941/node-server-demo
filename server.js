850db0a295d4b407bdb90850da496e9e52c5aeee
finley <sssrrr@vip.qq.com>
2018-03-26 19:37:43 +0800

    "简陋的本地数据库+登陆i注册+node.js"

--- server.js
+++ server.js
@@ -17,17 +17,93 @@ var server = http.createServer(function(request, response) {
     }
     var path = parsedUrl.pathname
     var query = parsedUrl.query
-  var method = request.method
-  
+    var method = request.method
+
     /******** 从这里开始看，上面不要看 ************/
 
     console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
     if (path === '/') {
+    let string = fs.readFileSync('./index.html', 'utf8')
+    let cookies =  request.headers.cookie.split('; ') // ['email=1@', 'a=1', 'b=2']
+    let hash = {}
+    for(let i =0;i<cookies.length; i++){
+      let parts = cookies[i].split('=')
+      let key = parts[0]
+      let value = parts[1]
+      hash[key] = value 
+    }
+    let email = hash.sign_in_email
+    let users = fs.readFileSync('./user.json', 'utf8')
+    users = JSON.parse(users)
+    let foundUser
+    for(let i=0; i< users.length; i++){
+      if(users[i].email === email){
+        foundUser = users[i]
+        break
+      }
+    }
+    console.log(foundUser)
+    if(foundUser){
+      string = string.replace('__password__', foundUser.password)
+    }else{
+      string = string.replace('__password__', '不知道')
+    }
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/sign_up' && method === 'GET'){ 
+    let string = fs.readFileSync('./sign_up.html', 'utf8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+    } else if (path === '/sign_in' && method === 'GET') {
+        let string = fs.readFileSync('./sign_in.html', 'utf8')
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write('哈哈哈')
+        response.write(string)
         response.end()
+    } else if (path === '/sign_in' && method === 'POST') {
+        readBody(request).then(body => {
+            let strings = body.split('&')
+            let hash = {}
+            strings.forEach(string => {
+                let parts = string.split('=')
+                let key = parts[0]
+                let value = parts[1]
+                hash[key] = decodeURIComponent(value)
+            })
+            let { email, password } = hash
+            let users = fs.readFileSync('./user.json', 'utf8')
+            try {
+                users = JSON.parse(users) //[]
+            } catch (error) {
+                users = []
+            }
+            let found
+            for (let i = 0; i < users.length; i++) {
+                if (
+                    users[i].email === email &&
+                    users[i].password === password
+                ) {
+                    found = true
+                    break
+                }
+            }
+            if (found) {
+                //'Set-Cookie: <cookie-name>=<cookie-value> '
+                response.setHeader(
+                    `Set-Cookie`,
+                    `sign_in_email=${email};HttpOnly`
+                )
+                response.statusCode = 200
+            } else {
+                response.statusCode = 401
+            }
+            response.end()
+        })
     } else if (path === '/sign_up' && method === 'GET') {
         let string = fs.readFileSync('./sign_up.html', 'utf8')
         response.statusCode = 200
@@ -42,17 +118,19 @@ var server = http.createServer(function(request, response) {
                 let parts = string.split('=')
                 let key = parts[0]
                 let value = parts[1]
-                hash[key] = value
+                hash[key] = decodeURIComponent(value)
             })
             // let email = hash['email']
             // let password = hash['password']
             // let password_confirmation = hash['password_confirmation']
-            console.log(hash)
             let { email, password, password_confirmation } = hash
-            if (email.indexOf('%40') === -1) {
+            if (email.indexOf('@') === -1) {
                 //%40===@ unicode编码
-              response.statusCode = 400
-                response.setHeader('Content-Type', 'application/json;charset=utf-8')
+                response.statusCode = 400
+                response.setHeader(
+                    'Content-Type',
+                    'application/json;charset=utf-8'
+                )
                 response.write(`{
                   "errors":{
                     "email":"invalid"
@@ -62,7 +140,32 @@ var server = http.createServer(function(request, response) {
                 response.statusCode = 400
                 response.write('password not match')
             } else {
-                response.statusCode = 200
+                let users = fs.readFileSync('./user.json', 'utf8')
+                try {
+                    users = JSON.parse(users) //[]
+                } catch (error) {
+                    users = []
+                }
+                let inUse = false
+                for (let i = 0; i < users.length; i++) {
+                    let user = users[i]
+                    if (user.email === email) {
+                        inUse = true
+                        break
+                    }
+                }
+                if (inUse) {
+                    response.statusCode = 400
+                    response.write('email in use')
+                } else {
+                    users.push({
+                        email: email,
+                        password: password
+                    })
+                    var userString = JSON.stringify(users)
+                    fs.writeFileSync('./user.json', userString)
+                    response.statusCode = 200
+                }
             }
             response.end()
         })
